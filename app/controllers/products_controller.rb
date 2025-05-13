230b486cf81b9d617736cd7183cffe4a4f2da56a
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-05-13 22:53:46 +0200

    Fill in products controller & view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,33 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ edit update destroy ]
+
+  def index
+    @products = Current.user.products.order(:name)
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to products_path, notice: "Product updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy!
+    redirect_to products_path, status: :see_other, notice: "Product destroyed"
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params.expect(:id))
+  end
+
+  def product_params
+    params.expect(product: [ :name, :calories, :protein, :fat, :carbs, :portion_type, :portion_size ])
+  end
 end
