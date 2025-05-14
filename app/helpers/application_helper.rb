78d78b228df5d0c6595d14db8f58ee489275622e
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-05-14 20:11:18 +0200

    Introduce DashboardController

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_link_to(name, url, controller)
+    classes = %w(material-icon p-2 hover:text-white rounded-sm)
+    classes += %w(text-white bg-lime-600) if params[:controller] == controller
+
+    link_to name, url, class: classes
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "products#index"
+  root "dashboard#show"
 
   resource :session
 
