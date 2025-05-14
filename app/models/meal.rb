7eae1bf791d5754dc43dd681a834374a146983eb
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-05-14 22:56:57 +0200

    Mock precalculated meal totals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,19 @@
 class Meal < ApplicationRecord
   belongs_to :user
+
+  def total_calories
+    605
+  end
+
+  def total_protein
+    30.5
+  end
+
+  def total_fat
+    19
+  end
+
+  def total_carbs
+    78.2
+  end
 end
