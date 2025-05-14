196fff6306163541c41e8418afca2f19964ab506
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-05-14 16:53:49 +0200

    Move "Add product" to header

--- app/javascript/controllers/page_controller.js
+++ app/javascript/controllers/page_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form"];
 
-  show(event) {
+  showForm(event) {
     event.preventDefault();
     this.formTarget.classList.remove("hidden");
     this.formTarget.querySelector('input:not([type="hidden"])')?.focus();
