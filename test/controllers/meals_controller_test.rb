3649e8295a44477a19b32ab3994d75e1bf1deb33
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-05-15 13:11:26 +0200

    Start tests

---
+++ test/controllers/meals_controller_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+class MealsControllerTest < ActionDispatch::IntegrationTest
+  def sign_in_as(user)
+    post session_url, params: { username: user.username, password: "password" }
+  end
+
+  test "requires authentication" do
+    get meals_url(date: "2025-05-15")
+    assert_redirected_to new_session_url
+  end
+
+  test "lists user's meals for given day" do
+    sign_in_as users(:joe)
+
+    target_date = meals(:joe_monday_breakfast).date
+    get meals_url(date: target_date.iso8601)
+
+    assert_response :success
+    content = response.parsed_body.content
+
+    # Contains all meals for this user for this day
+    assert_includes content, meals(:joe_monday_breakfast).name
+    assert_includes content, meals(:joe_monday_lunch).name
+
+    # Does not contain meals from other days or other users
+    assert_not_includes content, meals(:joe_tuesday_breakfast).name
+    assert_not_includes content, meals(:sue_monday_breakfast).name
+  end
+
+  test "redirects to root if date is invalid" do
+    sign_in_as users(:joe)
+    get meals_url(date: "invalid")
+    assert_redirected_to root_url
+  end
+end
---
+++ test/routes/meals_routes_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class MealsRoutesTest < ActionDispatch::IntegrationTest
+  test "root redirects to today's meals" do
+    get '/'
+    assert_redirected_to "/#{Date.today.iso8601}/meals"
+  end
+
+  test "day meals index" do
+    assert_routing '/2025-05-15/meals', { controller: 'meals', action: 'index', date: '2025-05-15' }
+  end
+
+  test "day meal details" do
+    assert_routing '/2025-05-15/meals/7', { controller: 'meals', action: 'show', date: '2025-05-15', id: '7' }
+  end
+end
