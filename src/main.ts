a77120cfda95801cc88cb9bfc99d30ee803f1332
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-06-24 00:42:14 +0200

    Initial UI

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
 import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <h1>Welcome to Foodie</h1>
+  <div class="pane">
+    <div class="tab-bar">
+      <span class="tab">Config</span>
+    </div>
+
+    <textarea placeholder="Your configuration" spellcheck="false" autocomplete="off" autocorrect="off" autocapitalize="off"></textarea>
+  </div>
+
+  <div class="separator"></div>
+
+  <div class="pane">
+    <div class="tab-bar">
+      <span class="tab">Today</span>
+    </div>
+
+    <textarea placeholder="Your day" spellcheck="false" autocomplete="off" autocorrect="off" autocapitalize="off"></textarea>
+
+    <div class="nutrition-stats"></div>
+  </div>
 `;
