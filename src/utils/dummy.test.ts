37823ffeb0e49e42e66a134e679df6dcb2acb6c7
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-06-24 23:28:26 +0200

    Remove dummy test

--- src/utils/dummy.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "vitest";
-
-describe("array join", () => {
-  it("should join array into greeting", () => {
-    const words = ["Hello", "world!"];
-    expect(words.join(", ")).toBe("Hello, world!");
-  });
-});
