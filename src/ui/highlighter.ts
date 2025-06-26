766300c71538706b25e89c29594846deac13cc39
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2025-06-26 19:23:20 +0200

    Shell for initHighlighter

--- src/ui/highlighter.ts
+++ src/ui/highlighter.ts
@@ -1,11 +1,43 @@
-export function initHighlighter(_textarea: HTMLTextAreaElement, _patterns: Record<string, RegExp>) {
-  // 1. Insert <pre class="editor-highlight"></pre>
-  //    as sibling of textarea
-  // 2. Define `highlight` function which constructs HTML
-  //    to put into the editor-highlight layer
-  //    (consider safe HTML insertion)
-  // 3. Add event handler: on `input` call `highlight`
-  // 4. Add event handler: on `scroll` match scroll position
-  // 5. Trigger initial highlight
-  // 6. Hide text in the textarea
+function escapeHtml(s: string) {
+  // prettier-ignore
+  return s
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
+export function highlightLine(line: string, _patterns: Record<string, RegExp>): string {
+  // TODO: Implement highlighting based on input patterns
+  return escapeHtml(line);
+}
+
+export function initHighlighter(textarea: HTMLTextAreaElement, patterns: Record<string, RegExp>) {
+  // Create the highlight layer element in DOM
+  const highlightLayer = document.createElement("pre");
+  highlightLayer.className = "editor-highlight";
+  textarea.parentElement!.insertBefore(highlightLayer, textarea);
+
+  const highlight = () => {
+    highlightLayer.innerHTML = textarea.value
+      .split("\n")
+      .map((line) => highlightLine(line, patterns))
+      .join("\n");
+  };
+
+  // Highlight on input
+  textarea.addEventListener("input", highlight);
+
+  // Synchronize scroll position with the highlight layer
+  textarea.addEventListener("scroll", () => {
+    highlightLayer.scrollTop = textarea.scrollTop;
+    highlightLayer.scrollLeft = textarea.scrollLeft;
+  });
+
+  // Trigger initial highlight
+  highlight();
+
+  // Highlight layer is present, we can hide textarea text
+  textarea.style.color = "transparent";
 }
